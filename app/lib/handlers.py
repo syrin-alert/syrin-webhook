58f2f5c0719bb4673b2700b193ff5ecd625294d5
DiegOps <didevlab@gmail.com>
2025-06-15 11:11:17 +0000

    refactor: ♻ refactor code

--- app/lib/handlers.py
+++ app/lib/handlers.py
@@ -12,32 +12,41 @@ def clean_description(description):
     description = re.sub(r'_', '-', description)
     return description
 
-def process_alertmanager_payload(data):
-    for alert in data['alerts']:
-        infra = alert.get('labels', {}).get('cluster', '(não identificado)').upper()
-        k8s = alert.get('labels', {}).get(SERVICE_K8S, '(não identificado)').upper()
-        if SERVICE_K8S == 'oke':
-            tenancy = alert.get('labels', {}).get('tenancy', '(não identificado)').lower()
-        namespace = alert.get('labels', {}).get('namespace', '(não identificado)').lower()
-        description = clean_description(alert.get('annotations', {}).get('description', 'No description provided'))
-        text = f"Ambiente: {infra}\n Tenancy: {tenancy}\n Kubernetes: {k8s}\n Namespace: {namespace}\n{description}"
-        severity = alert.get('labels', {}).get('severity', 'warning').lower()
-        level = 'critical' if severity in ['error', 'critical'] else 'warning'
+def extract_alert_info(alert):
+    labels = alert.get('labels', {})
+    annotations = alert.get('annotations', {})
+
+    infra = labels.get('cluster', '( unidentified )').upper()
+    k8s = labels.get(SERVICE_K8S, '( unidentified )').upper()
+    tenancy = labels.get('tenancy', '( unidentified )').lower() if SERVICE_K8S == 'oke' else ''
+    namespace = labels.get('namespace', '( unidentified )').lower()
+    description = clean_description(annotations.get('description', 'No description provided'))
+    summary = clean_description(annotations.get('summary', 'No summary provided'))
+    severity = labels.get('severity', 'warning').lower()
+    level = 'critical' if severity in ['error', 'critical'] else 'warning'
+
+    text = (
+        f"Environment: {infra}\n"
+        f"Tenancy: {tenancy}\n"
+        f"Kubernetes: {k8s}\n"
+        f"Namespace: {namespace}\n"
+        f"Description: {description}\n"
+        f"Summary: {summary}"
+    )
+
+    return text, level
+
+def process_alerts(alerts):
+    for alert in alerts:
+        text, level = extract_alert_info(alert)
         threading.Thread(target=send_text_to_queue, args=(text, level)).start()
+
+def process_alertmanager_payload(data):
+    process_alerts(data['alerts'])
     return jsonify({"message": "Alerts from Alertmanager processed successfully"}), 200
 
 def process_pod_alert_payload(data):
-    for alert in data:
-        infra = alert.get('labels', {}).get('cluster', '(não identificado)').upper()
-        k8s = alert.get('labels', {}).get(SERVICE_K8S, '(não identificado)').upper()
-        if SERVICE_K8S == 'oke':
-            tenancy = alert.get('labels', {}).get('tenancy', '(não identificado)').lower()
-        namespace = alert.get('labels', {}).get('namespace', '(não identificado)').lower()
-        description = clean_description(alert.get('annotations', {}).get('description', 'No description provided'))
-        text = f"Ambiente: {infra}\n Tenancy: {tenancy}\n Kubernetes: {k8s}\n Namespace: {namespace}\n{description}"
-        severity = alert.get('labels', {}).get('severity', 'warning').lower()
-        level = 'critical' if severity in ['error', 'critical'] else 'warning'
-        threading.Thread(target=send_text_to_queue, args=(text, level)).start()
+    process_alerts(data)
     return jsonify({"message": "Pod alerts processed successfully"}), 200
 
 def process_text_payload(data):
@@ -51,4 +60,4 @@ def process_text_payload(data):
         return jsonify({"critical": "No text field provided"}), 400
 
     threading.Thread(target=send_text_to_queue, args=(text, level)).start()
-    return jsonify({"message": f"Request received, processing in progress."}), 200
+    return jsonify({"message": "Request received, processing in progress."}), 200
